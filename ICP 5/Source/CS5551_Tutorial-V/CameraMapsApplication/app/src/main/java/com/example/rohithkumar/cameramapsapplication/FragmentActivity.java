837420580a6eb621c20f32a5d3f3473cbdad99f8
Prakashravella <42652316+Prakashravella@users.noreply.github.com>
2018-09-24 02:02:40 -0500

    Source Code

---
+++ ICP 5/Source/CS5551_Tutorial-V/CameraMapsApplication/app/src/main/java/com/example/rohithkumar/cameramapsapplication/FragmentActivity.java
@@ -0,0 +1,4 @@
+package com.example.rohithkumar.cameramapsapplication;
+
+class FragmentActivity {
+}
---
+++ ICP 5/Source/CS5551_Tutorial-V/CameraMapsApplication/app/src/main/java/com/example/rohithkumar/cameramapsapplication/OnMapReadyCallback.java
@@ -0,0 +1,4 @@
+package com.example.rohithkumar.cameramapsapplication;
+
+interface OnMapReadyCallback {
+}
