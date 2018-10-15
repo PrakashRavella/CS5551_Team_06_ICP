61684e26ba5ae47983edd72d710f9dbc9095b904
Prakashravella <42652316+Prakashravella@users.noreply.github.com>
2018-10-15 05:43:48 -0500

    ICP 8

---
+++ ICP 8/Source/Source/client/myapp/src/app/pages/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  dataSources:string[];
+  rxcui:string;
+  dataSourceSelected:string = 'DrugBank';
+  url:string;
+  result:Observable<any>;
+  constructor(private http: HttpClient) {
+    this.rxcui = "384455";
+    this.dataSources = ['ONCHigh','DrugBank'];
+  }
+  selectDataSource(src:string){
+    this.dataSourceSelected = src;
+  }
+  retriveData(){
+    this.url = 'http://127.0.0.1:3000/api/rxlist?rxcui='+this.rxcui+'&sources='+this.dataSourceSelected;
+    this.result = String[''];
+    this.http.get(this.url)
+      .subscribe(
+        (res:any)=>{
+          this.result = res.interactionTypeGroup[0].interactionType;
+          console.log(this.result);
+        }
+      )
+  }
+  ngOnInit() {
+  }
+
+}
